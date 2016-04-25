80af11cd92507a5bb0ed069c268e3fba7ab2b33b
光弘 <guanghong.wsj@alibaba-inc.com>
2016-04-25 16:47:46 +0800

    ver. 0.1.5

--- HISTORY.md
+++ HISTORY.md
@@ -1,3 +1,7 @@
+## 0.1.5
+
+`FIX` export logo.svg in build
+
 ## 0.1.3
 
 `NEW` add visual engine support
--- gulpfile.js
+++ gulpfile.js
@@ -75,6 +75,14 @@ gulp.task('pack_build', function(cb) {
         })
 });
 
+gulp.task('logo_build', function(cb) {
+    gulp.src(['./src/**/*.svg'])
+        .pipe(gulp.dest('build'))
+        .on('end', function() {
+            cb();
+        })
+})
+
 gulp.task('less_demo', function(cb) {
     gulp.src(['./demo/**/*.less'])
         .pipe(sourcemaps.init())
@@ -121,11 +129,11 @@ gulp.task('server', [
 
 });
 
-gulp.task('default', ['pack_build'], function() {
+gulp.task('default', ['pack_build', 'logo_build'], function() {
 
 });
 
-gulp.task('publish', ['pack_build'], function() {
+gulp.task('publish', ['pack_build', 'logo_build'], function() {
     setTimeout(function() {
         var questions = [
             {
@@ -179,5 +187,5 @@ gulp.task('publish', ['pack_build'], function() {
             spawn.sync(answers.npm, ['publish'], {stdio: 'inherit'});
         })
     }, 0)
-
+    
 });
